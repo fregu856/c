a3b2d282b74ec97034a09a3009ebf3e51074809a
fregu856 <fregu856@student.liu.se>
2017-11-12 14:00:26 +0100

    Assignment 2 seems to work.

--- assignment2/clock.c
+++ assignment2/clock.c
@@ -55,10 +55,103 @@ void clock_init(void)
 void clock_set_time(int hours, int minutes, int seconds)
 {
     pthread_mutex_lock(&Clock.mutex);
-
     Clock.time.hours = hours;
     Clock.time.minutes = minutes;
     Clock.time.seconds = seconds;
+    pthread_mutex_unlock(&Clock.mutex);
+}
+
+/* clock_increment_time: increments the current time with one second */
+void clock_increment_time(void)
+{
+    pthread_mutex_lock(&Clock.mutex);
+
+    /* increment time */
+    Clock.time.seconds++;
+    if (Clock.time.seconds > 59)
+    {
+        Clock.time.seconds = 0;
+        Clock.time.minutes++;
+        if (Clock.time.minutes > 59)
+        {
+            Clock.time.minutes = 0;
+            Clock.time.hours++;
+            if (Clock.time.hours > 23)
+            {
+                Clock.time.hours = 0;
+            }
+        }
+    }
 
     pthread_mutex_unlock(&Clock.mutex);
+}
+
+/* clock_get_time: get the current time */
+void clock_get_time(int *hours, int *minutes, int *seconds)
+{
+    pthread_mutex_lock(&Clock.mutex);
+    *hours = Clock.time.hours;
+    *minutes = Clock.time.minutes;
+    *seconds = Clock.time.seconds;
+    pthread_mutex_unlock(&Clock.mutex);
+}
+
+/* clock_set_alarm_time: set alarm time to hours, minutes and seconds */
+void clock_set_alarm_time(int hours, int minutes, int seconds)
+{
+    pthread_mutex_lock(&Clock.mutex);
+    Clock.alarm_time.hours = hours;
+    Clock.alarm_time.minutes = minutes;
+    Clock.alarm_time.seconds = seconds;
+    pthread_mutex_unlock(&Clock.mutex);
+}
+
+/* clock_get_alarm_time: get the alarm time */
+void clock_get_alarm_time(int *hours, int *minutes, int *seconds)
+{
+    pthread_mutex_lock(&Clock.mutex);
+    *hours = Clock.alarm_time.hours;
+    *minutes = Clock.alarm_time.minutes;
+    *seconds = Clock.alarm_time.seconds;
+    pthread_mutex_unlock(&Clock.mutex);
+}
+
+/* clock_enable_alarm: enable alarm */
+void clock_enable_alarm(void)
+{
+  pthread_mutex_lock(&Clock.mutex);
+  Clock.alarm_enabled = 1;
+  pthread_mutex_unlock(&Clock.mutex);
+}
+
+/* clock_alarm_enabled: returns 1 if the alarm is enabled */
+int clock_alarm_enabled(void)
+{
+    int alarm_enabled;
+
+    pthread_mutex_lock(&Clock.mutex);
+    alarm_enabled = Clock.alarm_enabled;
+    pthread_mutex_unlock(&Clock.mutex);
+
+    return alarm_enabled;
+}
+
+/* clock_disable_alarm: disable alarm */
+void clock_disable_alarm(void)
+{
+  pthread_mutex_lock(&Clock.mutex);
+  Clock.alarm_enabled = 0;
+  pthread_mutex_unlock(&Clock.mutex);
+}
+
+/* clock_activation_signal: signal that the alarm shall be activated */
+void clock_activation_signal(void)
+{
+    sem_post(&Clock.start_alarm);
+}
+
+/* clock_activation_wait: wait for the alarm to be activated */
+void clock_activation_wait(void)
+{
+    sem_wait(&Clock.start_alarm);
 }
--- assignment2/set_clock.c
+++ assignment2/set_clock.c
@@ -1,9 +1,5 @@
-/* A clock program
-
-Two tasks are used, clock_task for updating a clock, and
-set_task for setting the clock
-
-*/
+// Three threads are used, clock_thread for updating a clock, alarm_thread for
+// handling alarm functionality, and GUI_thread for reading user input
 
 /* standard library includes */
 #include <string.h>
@@ -17,58 +13,22 @@ set_task for setting the clock
 #include "display.h"
 #include "clock.h"
 
-/* clock variables */
-
-/* functions operating on the clock */
-
-/* increment_time: increments the current time with
-   one second */
-void increment_time(void)
-{
-    /* reserve clock variables */
-    pthread_mutex_lock(&Mutex);
-
-    /* increment time */
-    Seconds++;
-    if (Seconds > 59)
-    {
-        Seconds = 0;
-        Minutes++;
-        if (Minutes > 59)
-        {
-            Minutes = 0;
-            Hours++;
-            if (Hours > 23)
-            {
-                Hours = 0;
-            }
-        }
-    }
-
-    /* release clock variables */
-    pthread_mutex_unlock(&Mutex);
-}
-
-/* get_time: read time from common clock variables */
-void get_time(int *hours, int *minutes, int *seconds)
-{
-    /* reserve clock variables */
-    pthread_mutex_lock(&Mutex);
-
-    /* read values */
-    *hours = Hours;
-    *minutes = Minutes;
-    *seconds = Seconds;
-
-    /* release clock variables */
-    pthread_mutex_unlock(&Mutex);
-}
+////////////////////////////////////////////////////////////////////////////////
+// helper functions
+////////////////////////////////////////////////////////////////////////////////
 
 /* time_from_set_time_message: extract time from set time
    message from user interface */
 void time_from_set_time_message(char message[], int *hours, int *minutes, int *seconds)
 {
-    sscanf(message,"set %d %d %d", hours, minutes, seconds);
+    sscanf(message, "set %d %d %d", hours, minutes, seconds);
+}
+
+/* time_from_alarm_time_message: extract time from alarm time
+   message from user interface */
+void time_from_alarm_time_message(char message[], int *hours, int *minutes, int *seconds)
+{
+    sscanf(message, "alarm %d %d %d", hours, minutes, seconds);
 }
 
 /* time_ok: returns nonzero if hours, minutes and seconds represents a valid time */
@@ -78,7 +38,9 @@ int time_ok(int hours, int minutes, int seconds)
         seconds >= 0 && seconds <= 59;
 }
 
-/* tasks */
+////////////////////////////////////////////////////////////////////////////////
+// threads
+////////////////////////////////////////////////////////////////////////////////
 
 /* clock_thread: clock thread */
 void* clock_thread(void *unused)
@@ -86,15 +48,27 @@ void* clock_thread(void *unused)
     /* local copies of the current time */
     int hours, minutes, seconds;
 
+    // local copies of the alarm time:
+    int alarm_hours, alarm_minutes, alarm_seconds;
+
     /* infinite loop */
     while (1)
     {
         /* read and display current time */
-        get_time(&hours, &minutes, &seconds);
+        clock_get_time(&hours, &minutes, &seconds);
         display_time(hours, minutes, seconds);
 
+        if (clock_alarm_enabled())
+        {
+            clock_get_alarm_time(&alarm_hours, &alarm_minutes, &alarm_seconds);
+            if (hours == alarm_hours && minutes == alarm_minutes && seconds == alarm_seconds)
+            {
+              clock_activation_signal();
+            }
+        }
+
         /* increment time */
-        increment_time();
+        clock_increment_time();
 
         /* wait one second */
         usleep(1000000);
@@ -107,7 +81,15 @@ void* alarm_thread(void *unused)
     /* infinite loop */
     while (1)
     {
+        clock_activation_wait();
 
+        while(clock_alarm_enabled())
+        {
+            display_alarm_text();
+
+            // wait 1.5 seconds:
+            usleep(1500000);
+        }
     }
 }
 
@@ -128,18 +110,43 @@ void* GUI_thread(void *unused)
     {
         /* read a message */
         si_ui_receive(message);
-        /* check if it is a set message */
+
+        /* check if it's a set message */
         if (strncmp(message, "set", 3) == 0)
         {
-            time_from_set_message(message, &hours, &minutes, &seconds);
+            time_from_set_time_message(message, &hours, &minutes, &seconds);
             if (time_ok(hours, minutes, seconds))
             {
-                set_time(hours, minutes, seconds);
+                clock_set_time(hours, minutes, seconds);
             }
             else
             {
-                si_ui_show_error("Illegal value for hours, minutes or seconds");
+                si_ui_show_error("set: Illegal value for hours, minutes or seconds");
             }
+        }
+        // check if it's an alarm message:
+        else if (strncmp(message, "alarm", 5) == 0)
+        {
+            time_from_alarm_time_message(message, &hours, &minutes, &seconds);
+            if (time_ok(hours, minutes, seconds))
+            {
+                clock_set_alarm_time(hours, minutes, seconds);
+                clock_enable_alarm();
+
+                display_alarm_time(hours, minutes, seconds);
+            }
+            else
+            {
+                si_ui_show_error("alarm: Illegal value for hours, minutes or seconds");
+            }
+        }
+        // check if it's a reset message:
+        else if (strcmp(message, "reset") == 0)
+        {
+            // acknowledge the alarm:
+            clock_disable_alarm();
+            erase_alarm_time();
+            erase_alarm_text();
         }
         /* check if it is an exit message */
         else if (strcmp(message, "exit") == 0)
@@ -154,24 +161,33 @@ void* GUI_thread(void *unused)
     }
 }
 
-/* main */
+////////////////////////////////////////////////////////////////////////////////
+// main
+////////////////////////////////////////////////////////////////////////////////
+
 int main(void)
 {
-    /* initialise UI channel */
+    // initialize UI channel:
     si_ui_init();
 
-    /* initialise variables */
-    init_clock();
+    // initialize display:
+    display_init();
 
-    /* create tasks */
+    // initialize clock:
+    clock_init();
+
+    // create threads:
     pthread_t clock_thread_handle;
+    pthread_t alarm_thread_handle;
     pthread_t GUI_thread_handle;
-
     pthread_create(&clock_thread_handle, NULL, clock_thread, 0);
+    pthread_create(&alarm_thread_handle, NULL, alarm_thread, 0);
     pthread_create(&GUI_thread_handle, NULL, GUI_thread, 0);
 
+    // join threads:
     pthread_join(clock_thread_handle, NULL);
+    pthread_join(alarm_thread_handle, NULL);
     pthread_join(GUI_thread_handle, NULL);
-    /* will never be here! */
+
     return 0;
 }
