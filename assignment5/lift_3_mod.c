e8002fa3ead5c4b15c31a5eb694e305200c273ba
fregu856 <fregu856@student.liu.se>
2017-12-11 11:14:34 +0100

    >

---
+++ assignment5/lift_3_mod.c
@@ -0,0 +1,441 @@
+#include "lift_3_mod.h" ////////////////////////////////////////////////////////////////
+
+/* Simple_OS include */
+#include <pthread.h>
+
+/* drawing module */
+#include "draw_3.h"
+
+/* standard includes */
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/* panic function, to be called when fatal errors occur */
+static void lift_panic(const char message[])
+{
+    printf("LIFT_PANIC!!! ");
+    printf("%s", message);
+    printf("\n");
+    exit(0);
+}
+
+/* --- monitor data type for lift and operations for create and delete START --- */
+
+/* lift_create: creates and initialises a variable of type lift_type */
+lift_type lift_create(void)
+{
+    /* the lift to be initialised */
+    lift_type lift;
+
+    /* floor counter */
+    int floor;
+
+    /* loop counter */
+    int i;
+
+    /* allocate memory */
+    lift = (lift_type) malloc(sizeof(lift_data_type));
+
+    /* initialise variables */
+
+    /* initialise floor */
+    lift->floor = 0;
+
+    /* set direction of lift travel to up */
+    lift->up = 1;
+
+    /* the lift is not moving */
+    lift->moving = 0;
+
+    /* initialise person information */
+    for (floor = 0; floor < N_FLOORS; floor++)
+    {
+        for (i = 0; i < MAX_N_PERSONS; i++)
+        {
+            lift->persons_to_enter[floor][i].id = NO_ID;
+            lift->persons_to_enter[floor][i].to_floor = NO_FLOOR;
+        }
+    }
+
+    /* initialise passenger information */
+    for (i = 0; i < MAX_N_PASSENGERS; i++)
+    {
+        lift->passengers_in_lift[i].id = NO_ID;
+        lift->passengers_in_lift[i].to_floor = NO_FLOOR;
+    }
+
+    /* initialise mutex and event variable */
+    pthread_mutex_init(&lift->mutex, NULL);
+    pthread_cond_init(&lift->change, NULL);
+
+    return lift;
+}
+
+/* lift_delete: deallocates memory for lift */
+void lift_delete(lift_type lift)
+{
+    free(lift);
+}
+
+/* --- monitor data type for lift and operations for create and delete END --- */
+
+
+/* --- functions related to lift task START --- */
+
+/* MONITOR function lift_next_floor: computes the floor to which the lift
+   shall travel. The parameter *change_direction indicates if the direction
+   shall be changed */
+void lift_next_floor(lift_type lift, int *next_floor, int *change_direction) /////////////////////////
+{
+    int floor;
+    int up;
+
+    pthread_mutex_lock(&lift->mutex);
+    floor = lift->floor;
+    up = lift->up;
+    pthread_mutex_unlock(&lift->mutex);
+
+    if (floor == 0)
+    {
+        *next_floor = 1;
+        *change_direction = 1;
+    }
+    else if (floor == N_FLOORS - 1)
+    {
+        *next_floor = N_FLOORS - 2;
+        *change_direction = 1;
+    }
+    else if (up)
+    {
+        *next_floor = floor + 1;
+        *change_direction = 0;
+    }
+    else // (if (!up))
+    {
+        *next_floor = floor - 1;
+        *change_direction = 0;
+    }
+}
+
+void lift_move(lift_type lift, int next_floor, int change_direction)
+{
+    /* reserve lift */
+    pthread_mutex_lock(&lift->mutex);
+
+    /* the lift is moving */
+    lift->moving = 1;
+
+    /* release lift */
+    pthread_mutex_unlock(&lift->mutex);
+
+    /* it takes two seconds to move to the next floor */
+    //usleep(2000000);
+
+    /* reserve lift */
+    pthread_mutex_lock(&lift->mutex);
+
+    /* the lift is not moving */
+    lift->moving = 0;
+
+    /* the lift has arrived at next_floor */
+    lift->floor = next_floor;
+
+    /* check if direction shall be changed */
+    if (change_direction)
+    {
+        lift->up = !lift->up;
+    }
+
+    /* draw, since a change has occurred */
+    //draw_lift(lift);
+
+    /* release lift */
+    pthread_mutex_unlock(&lift->mutex);
+}
+
+/* this function is used also by the person tasks */
+static int n_passengers_in_lift(lift_type lift)
+{
+    int n_passengers = 0;
+    int i;
+
+    for (i = 0; i < MAX_N_PASSENGERS; i++)
+    {
+        if (lift->passengers_in_lift[i].id != NO_ID)
+        {
+            n_passengers++;
+        }
+    }
+    return n_passengers;
+}
+
+// possible_to_enter_lift: returns 1 if there is room for at least one more
+// passenger to enter the lift, otherwise returns 0: ///////////////////////////////////////////////////
+static int possible_to_enter_lift(lift_type lift)
+{
+    if (n_passengers_in_lift(lift) < MAX_N_PASSENGERS)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+// passengers_to_enter_lift: returns 1 if there is at least one passenger
+// waiting to enter the lift on the current floor, otherwise returns 0: /////////////////////////////////////////
+static int passengers_to_enter_lift(lift_type lift)
+{
+    int current_floor = lift->floor;
+
+    int person_found = 0;
+    int i;
+    for (i=0; i < MAX_N_PERSONS && !person_found; i++)
+    {
+        if (lift->persons_to_enter[current_floor][i].id != NO_ID)
+        {
+            person_found = 1;
+        }
+    }
+
+    return person_found;
+}
+
+// passengers_to_exit_lift: returns 1 if there is at least one passenger in the
+// lift that shall exit the lift on the current floor, otherwise returns 0: /////////////////////////////////////////
+static int passengers_to_exit_lift(lift_type lift)
+{
+    int current_floor = lift->floor;
+
+    int person_found = 0;
+    int i;
+    for (i = 0; i < MAX_N_PASSENGERS && !person_found; i++)
+    {
+        if(lift->passengers_in_lift[i].to_floor == current_floor)
+        {
+            person_found = 1;
+        }
+    }
+
+    return person_found;
+}
+
+// lift_wait_for_passengers: returns 1 if the lift shall wait for
+// passengers to enter/leave the lift, otherwise returns 0: /////////////////////////////////////////
+static int lift_wait_for_passengers(lift_type lift)
+{
+    if (passengers_to_exit_lift(lift))
+    {
+        return 1;
+    }
+    else if (passengers_to_enter_lift(lift) && possible_to_enter_lift(lift))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+/* MONITOR function lift_has_arrived: shall be called by the lift task
+   when the lift has arrived at the next floor. This function indicates
+   to other tasks that the lift has arrived, and then waits until the lift
+   shall move again. */
+void lift_has_arrived(lift_type lift) ////////////////////////////////////////////////
+{
+    pthread_mutex_lock(&lift->mutex);
+
+    // signal that the lift has arrived at the next floor:
+    pthread_cond_broadcast(&lift->change);
+
+    while(lift_wait_for_passengers(lift))
+    {
+        pthread_cond_wait(&lift->change, &lift->mutex);
+    }
+
+    pthread_mutex_unlock(&lift->mutex);
+}
+
+/* --- functions related to lift task END --- */
+
+
+/* --- functions related to person task START --- */
+
+/* passenger_wait_for_lift: returns non-zero if the passenger shall
+   wait for the lift, otherwise returns zero */
+static int passenger_wait_for_lift(lift_type lift, int wait_floor)
+{
+    int waiting_ready =
+        /* the lift is not moving */
+        !lift->moving &&
+        /* and the lift is at wait_floor */
+        lift->floor == wait_floor &&
+        /* and the lift is not full */
+        n_passengers_in_lift(lift) < MAX_N_PASSENGERS;
+
+    return !waiting_ready;
+}
+
+// passenger_wait_for_exit: returns non-zero if the passenger shall wait for the
+// lift to arrive at his/her destination floor, otherwise returns zero: ////////////////////////////////
+static int passenger_wait_for_exit(lift_type lift, int destination_floor)
+{
+    int waiting_ready =
+        /* the lift is not moving */
+        !lift->moving &&
+        /* and the lift is at destination_floor */
+        lift->floor == destination_floor;
+
+    return !waiting_ready;
+}
+
+/* enter_floor: makes a person with id id stand at floor floor */
+static void enter_floor(lift_type lift, int id, int floor)
+{
+    int i;
+    int floor_index;
+    int found;
+
+    /* stand at floor */
+    found = 0;
+    for (i = 0; i < MAX_N_PERSONS && !found; i++)
+    {
+        if (lift->persons_to_enter[floor][i].id == NO_ID)
+        {
+            found = 1;
+            floor_index = i;
+        }
+    }
+
+    if (!found)
+    {
+        lift_panic("cannot enter floor");
+    }
+
+    /* enter floor at index floor_index */
+    lift->persons_to_enter[floor][floor_index].id = id;
+    lift->persons_to_enter[floor][floor_index].to_floor = NO_FLOOR;
+}
+
+/* leave_floor: makes a person with id id at enter_floor leave
+   enter_floor */
+static void leave_floor(lift_type lift, int id, int enter_floor)
+/* fig_end lift_c_prot */
+{
+    int i;
+    int floor_index;
+    int found;
+
+    /* leave the floor */
+    found = 0;
+    for (i = 0; i < MAX_N_PERSONS && !found; i++)
+    {
+        if (lift->persons_to_enter[enter_floor][i].id == id)
+        {
+            found = 1;
+            floor_index = i;
+        }
+    }
+
+    if (!found)
+    {
+        lift_panic("cannot leave floor");
+    }
+
+    /* leave floor at index floor_index */
+    lift->persons_to_enter[enter_floor][floor_index].id = NO_ID;
+    lift->persons_to_enter[enter_floor][floor_index].to_floor = NO_FLOOR;
+}
+
+// enter_lift: makes person *person enter the lift: /////////////////////////////////////////////////////////
+static void enter_lift(lift_type lift, person_type person)
+{
+    int found_empty, passenger_index;
+
+    found_empty = 0;
+    int i;
+    for (i = 0; i < MAX_N_PASSENGERS && !found_empty; i++)
+    {
+        if (lift->passengers_in_lift[i].id == NO_ID) // (if empty spot in the lift)
+        {
+            found_empty = 1;
+            passenger_index = i;
+        }
+    }
+
+    if (!found_empty)
+    {
+        lift_panic("cannot enter the lift!");
+    }
+
+    lift->passengers_in_lift[passenger_index] = *person;
+}
+
+// leave_lift: makes person *person leave the lift: /////////////////////////////////////////////////////////
+static void leave_lift(lift_type lift, person_type person)
+{
+    int found, passenger_index;
+
+    found = 0;
+    int i = 0;
+    for (i = 0; i < MAX_N_PASSENGERS && !found; i++)
+    {
+        if (lift->passengers_in_lift[i].id == person->id)
+        {
+            found = 1;
+            passenger_index = i;
+        }
+    }
+
+    if (!found)
+    {
+        lift_panic("cannot leave the lift since the passenger wasn't found!");
+    }
+
+    lift->passengers_in_lift[passenger_index].id = NO_ID;
+    lift->passengers_in_lift[passenger_index].to_floor = NO_FLOOR;
+}
+
+/* MONITOR function lift_travel: performs a journey with the lift
+   starting at from_floor, and ending at to_floor */
+void lift_travel(lift_type lift, int id, int from_floor, int to_floor) ///////////////////////////////
+{
+    person_data_type person;
+    person.id = id;
+    person.to_floor = to_floor;
+
+    pthread_mutex_lock(&lift->mutex);
+
+    enter_floor(lift, id, from_floor);
+    pthread_cond_broadcast(&lift->change); // kanske kan strunta i denna????!!111!!
+
+    //draw_lift(lift);
+
+    while(passenger_wait_for_lift(lift, from_floor))
+    {
+        pthread_cond_wait(&lift->change, &lift->mutex);
+    }
+
+    leave_floor(lift, id, from_floor);
+    enter_lift(lift, &person);
+    pthread_cond_broadcast(&lift->change);
+
+    //draw_lift(lift);
+
+    while(passenger_wait_for_exit(lift, to_floor))
+    {
+        pthread_cond_wait(&lift->change, &lift->mutex);
+    }
+
+    leave_lift(lift, &person);
+    pthread_cond_broadcast(&lift->change);
+
+    //draw_lift(lift);
+
+    pthread_mutex_unlock(&lift->mutex);
+}
+
+/* --- functions related to person task END --- */
---
+++ assignment5/main_3_mod.c
@@ -0,0 +1,257 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <sys/prctl.h>
+
+#include "lift_3_mod.h" ////////////////////////////////////////////////////////////////////////
+#include "si_ui.h"
+#include "debug.h"
+
+#include <sys/time.h>
+
+// Unfortunately the rand() function is not thread-safe. However, the
+// rand_r() function is thread-safe, but need a pointer to an int to
+// store the current state of the pseudo-random generator.  This
+// pointer needs to be unique for every thread that might call
+// rand_r() simultaneously. The functions below are wrappers around
+// rand_r() which should work in the environment encountered in
+// assignment 3.
+//
+
+#define NUM_ITERATIONS 10000
+
+int current_passenger_id = 0;
+
+long long int run_times[MAX_N_PERSONS];
+
+// semaphore for asymmetric synchronization (unique id to person threads) /////////////////////////
+sem_t id_read;
+
+// array of passenger thread handles: //////////////////////////////////////////////////////
+pthread_t passenger_thread_handles[MAX_N_PERSONS];
+
+static unsigned int rand_r_state[MAX_N_PERSONS];
+
+// Get a random value between 0 and maximum_value. The passenger_id
+// parameter is used to ensure that the rand_r() function is called in
+// a thread-safe manner.
+static int get_random_value(int passenger_id, int maximum_value)
+{
+		return rand_r(&rand_r_state[passenger_id]) % (maximum_value + 1);
+}
+
+static lift_type Lift;
+
+// Initialize the random seeds used by the get_random_value() function
+// above.
+static void init_random(void)
+{
+			int i;
+			prctl(PR_SET_NAME,"Lift Thread",0,0,0); // Sets a name shown in debuggers
+
+			for(i=0; i < MAX_N_PERSONS; i++)
+			{
+					// Use this statement if the same random number sequence
+					// shall be used for every execution of the program.
+					rand_r_state[i] = i;
+
+					// Use this statement if the random number sequence
+					// shall differ between different runs of the
+					// program. (As the current time (as measured in
+					// seconds) is used as a seed.)
+					rand_r_state[i] = i+time(NULL);
+		}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// threads
+////////////////////////////////////////////////////////////////////////////////
+
+static void* lift_thread(void *unused) /////////////////////////////////////////
+{
+		int next_floor, change_direction;
+
+		while(1)
+		{
+				// compute which floor the lift should move to next:
+				lift_next_floor(Lift, &next_floor, &change_direction);
+
+				// move the lift to the next floor:
+				lift_move(Lift, next_floor, change_direction);
+
+				// signal that the lift has arrived at the next floor, wait until the
+				// lift shall move again:
+				lift_has_arrived(Lift);
+		}
+
+		return NULL;
+}
+
+static void* passenger_thread(void *idptr) /////////////////////////////////////
+{
+		// Code that reads the passenger ID from the idptr pointer
+		// (due to the way pthread_create works we first need to cast
+		// the void pointer to an int pointer).
+		int *tmp = (int*) idptr;
+		int id = *tmp;
+
+		// signal that it's safe to modify *idptr: ////////////////////////////////////////////////
+		sem_post(&id_read);
+
+	  // Sets a unique name shown in debuggers
+	  char buf[100];
+	  sprintf(buf, "Passenger #%d", id);
+		prctl(PR_SET_NAME,buf,0,0,0);
+
+		int from_floor, to_floor;
+
+		int num_iterations = 0;
+
+		struct timeval starttime;
+    struct timeval endtime;
+    long long int timediff;
+    gettimeofday(&starttime, NULL);
+
+		while(1)
+		{
+				if (num_iterations == NUM_ITERATIONS)
+				{
+						gettimeofday(&endtime, NULL);
+						timediff = (endtime.tv_sec*1000000ULL + endtime.tv_usec) -
+											 (starttime.tv_sec*1000000ULL + starttime.tv_usec);
+						printf("  time difference: %lld\n", timediff);
+
+						run_times[id] = timediff;
+
+						return;
+				}
+
+				from_floor = get_random_value(id, N_FLOORS-1);
+				to_floor = get_random_value(id, N_FLOORS-1);
+
+				//debug_check_override(id, &from_floor, &to_floor);
+				lift_travel(Lift, id, from_floor, to_floor);
+
+				// sleep for 5 seconds:
+				//usleep(5000000);
+
+				num_iterations++;
+		}
+
+		return NULL;
+}
+
+static void* user_thread(void *unused) /////////////////////////////////////////
+{
+		char message[SI_UI_MAX_MESSAGE_SIZE];
+
+		si_ui_set_size(670, 700);
+		prctl(PR_SET_NAME,"User Thread",0,0,0); // Sets the name shown in debuggers for this thread
+
+		while(1)
+		{
+				// Read a message from the GUI
+				si_ui_receive(message);
+
+				if(!strcmp(message, "exit"))
+				{
+						lift_delete(Lift);
+						exit(0);
+				}
+				else if(!strcmp(message, "pause"))
+				{
+						debug_pause();
+				}
+				else if(!strcmp(message, "unpause"))
+				{
+						debug_unpause();
+				}
+				else if(!strcmp(message, "test"))
+				{
+					// Test full hiss
+					debug_override(0, 0, 4);
+					debug_override(1, 0, 4);
+					debug_override(2, 0, 4);
+					debug_override(3, 0, 4);
+					debug_override(4, 0, 4);
+					debug_override(5, 2, 4);
+					debug_override(6, 2, 4);
+					debug_override(7, 2, 4);
+					debug_override(8, 2, 4);
+					debug_override(9, 2, 4);
+				}
+				else if(!strcmp(message, "test2"))
+				{
+					// Test
+					debug_override(0, 0, 0);
+					debug_override(1, 0, 0);
+					debug_override(2, 0, 0);
+					debug_override(3, 0, 0);
+					debug_override(4, 0, 0);
+					debug_override(5, 2, 2);
+					debug_override(6, 2, 2);
+					debug_override(7, 2, 2);
+					debug_override(8, 2, 2);
+					debug_override(9, 2, 2);
+				}
+		}
+
+		return NULL;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// main
+////////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char **argv) ////////////////////////////////////////////////
+{
+		si_ui_init();
+		debug_init();
+
+		init_random();
+
+		Lift = lift_create();
+
+		// create threads:
+		pthread_t lift_thread_handle;
+		pthread_t user_thread_handle;
+		pthread_create(&lift_thread_handle, NULL, lift_thread, 0);
+		pthread_create(&user_thread_handle, NULL, user_thread, 0);
+
+		// create MAX_N_PERSONS passenger threads:
+		// // initialize the semaphore for asymmetric synchronization:
+		sem_init(&id_read, 0, 0);
+		int i;
+		for(i = 0; i < MAX_N_PERSONS; i++)
+		{
+				pthread_create(&passenger_thread_handles[current_passenger_id],
+								NULL, passenger_thread, (void*) &current_passenger_id);
+
+				// wait for current_passenger_id to be read by the passenger thread:
+				sem_wait(&id_read);
+
+				current_passenger_id++;
+		}
+
+		// join threads:
+		for (i = 0; i < MAX_N_PERSONS; ++i)
+		{
+				pthread_join(passenger_thread_handles[i], NULL);
+		}
+
+		long long int sum = 0;
+		for (i = 0; i < MAX_N_PERSONS; ++i)
+		{
+				sum += run_times[i];
+		}
+		long long int average_run_times = sum/MAX_N_PERSONS;
+		printf("  average run time: %lld\n", average_run_times);
+
+		pthread_join(lift_thread_handle, NULL);
+		pthread_join(user_thread_handle, NULL);
+
+		return 0;
+}
